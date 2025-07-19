b2442956bf279e1edef89ad71f84bd4a72d72772
CalsonicSSS <calson.developer@gmail.com>
2025-07-19 15:25:17 -0400

    notes updates

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -24,11 +24,20 @@ export const metadata: Metadata = {
 };
 
 // The root layout will only be rendered once, and its children is only the current navigated route page rendered (all other are not rendered / executed) at a time (very efficient).
-// we have checked that any component that initializes context and any components that consumes context as subscriber, all must be client components
-// we have also checked that if any child component passed as children prop under the parent component, then:
-// 1. Children props don't re-render when parent re-renders
+// we have checked that any component that need to initialize react context and any components that consumes context as subscriber, all must be client components
+// we have also checked that if any child component passed as "children prop" under the parent component, then:
+// 1. Children components don't auto re-render when parent re-renders by default
 // 2. if children component is SC, then it will continue to be SC (this is very important to realize)
 // above two rules will not be the case if child component is directly passed / called under parent through import
+
+// what we have tested:
+// - a child login comopnent (under home page component) that consumes context for user login state update (this must be client component)
+// - this will trigger the top level AuthProvider component (must be client component under root layout) to re-render
+// - only this login component, AuthProvider and componets inside AuthProvider will re-render
+// what DO NOT re-render:
+//    - all other server components and other page route server component
+//    - all other client components that are not directly imported under the AuthProvider component nor they are the context subscribers
+// all server component remains the same.
 
 // so now we can make some conclusions
 // this ClerkProvider should be a client component, because it initializes context
